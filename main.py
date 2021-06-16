4596580e3e869e27090294d435a56828f7b2290d
adikusuma1402 <adikusuma1402@gmail.com>
2021-06-16 17:56:53 +0700

    update main.py, save dataframe to csv format

--- main.py
+++ main.py
@@ -25,3 +25,26 @@ heading[13] = 'Tests/1M pop'
 #mengekpor ke dataframe
 df_corona = pd.DataFrame(columns=heading)
 
+#mengisi tabel df_corona
+for j in table.find_all('tr')[1:]:
+    data_baris = j.find_all('td')
+    baris = [tr.text for tr in data_baris]
+    panjang = len(df_corona)
+    df_corona.loc[panjang] = baris
+
+#delete baris 0 - 6
+df_corona.drop(df_corona.index[0:7], inplace=True)
+#delete baris 222 - 228
+df_corona.drop(df_corona.index[222:229], inplace=True)
+
+#reset index agar mulai dari 0
+df_corona.reset_index(inplace=True, drop=True)
+
+#menghilangkan kolom #
+df_corona.drop('#', axis=1, inplace=True)
+
+
+#save dataframe to format csv
+df_corona.to_csv('data_corona.csv', index=False)
+
+
